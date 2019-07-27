7f4315052e7bd60f52e27105960974f373cbfc13
David Little <littledavid175@gmail.com>
2019-07-27 20:57:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,11 @@ require "pry"
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
-  name_hash.each do |name, num|
+name_hash.each do |name, num|
+   if lowest_value === nil|| num < lowest_value
+     lowest_value = num
+     lowest_key = name
+   end
   end
+  lowest_key
 end
